fec74cb7922336886aad6004dab31c7afa85236e
Vanessa V <vanevb976@gmail.com>
2021-09-03 15:11:35 -0500

    install vue router

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import { createProvider } from './vue-apollo'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   apolloProvider: createProvider(),
+  router,
   render: h => h(App)
 }).$mount('#app')
