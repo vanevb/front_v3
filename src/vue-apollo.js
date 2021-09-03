db6d0303543bef38ed2f7d39471af820b0b60a8e
Vanessa V <vanevb976@gmail.com>
2021-09-03 14:22:01 -0500

    Fix url API

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -9,7 +9,7 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://apigatewaylibreria.herokuapp.com/',
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://apigatewaylibreria.herokuapp.com/'
 // Files URL root
 export const filesRoot = process.env.VUE_APP_FILES_ROOT || httpEndpoint.substr(0, httpEndpoint.indexOf('/graphql'))
 
